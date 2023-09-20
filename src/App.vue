16f81bbd99b833502c40551c7389cd9a19fb5494
Ruan Bindaco <ruanazsxdc123@gmail.com>
2023-09-20 10:18:48 -0300

    Adding, deleting and showing saved cookies

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,30 @@
   <div id="app">
     <h1>Vue cookies</h1>
     <button @click="setCookie">Set Cookie</button>
+    <button @click="deleteCookie">Delete Cookie</button>
+    <div>Cookies: {{ cookiesSalvos }}</div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      cookiesSalvos: [],
+    };
+  },
   methods: {
     setCookie() {
       // it sets the cookie called `username`
       // this.$cookies.set("username", "gowtham");
-      this.$cookies.set(
-        "use_path_argument",
-        "value",
-        null,
-        null,
-        "dominio.com"
-      );
-      this.$cookies.set("token", "domain.com");
+      this.$cookies.set("cookieKeyName", "value", null, null, "dominio.com");
+      this.$cookies.set("token", "value");
+      this.cookiesSalvos = this.$cookies.keys();
     },
-    verifyDomain() {},
-  },
-  mounted() {
-    console.log(this.$cookies.get("token"));
-    // this.$cookies.keys().forEach((cookie) => this.$cookies.remove(cookie));
+    deleteCookie() {
+      this.$cookies.keys().forEach((cookie) => this.$cookies.remove(cookie));
+      this.cookiesSalvos = this.$cookies.keys();
+    },
   },
 };
 </script>
